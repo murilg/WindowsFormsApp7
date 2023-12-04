f15f20178381b0394d77198bb777130500a95f98
Артур <mirgaliev.a@yandex.ru>
2023-12-05 00:14:33 +0500

    обновление 1.1

--- WindowsFormsApp7/Form3.Designer.cs
+++ WindowsFormsApp7/Form3.Designer.cs
@@ -191,6 +191,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(188, 21);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
@@ -199,6 +200,7 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(188, 21);
             this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // textBox1
             // 
@@ -206,6 +208,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(188, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -213,6 +216,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(188, 20);
             this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
@@ -222,6 +226,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Назад";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form3
             // 
--- WindowsFormsApp7/Form3.cs
+++ WindowsFormsApp7/Form3.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApp7
 {
     public partial class Form3 : Form
     {
+        BindingSource Sbind = new BindingSource();
         public Form3()
         {
             InitializeComponent();
@@ -21,7 +22,130 @@ namespace WindowsFormsApp7
         {
             // TODO: данная строка кода позволяет загрузить данные в таблицу "rTADataSet.Driver". При необходимости она может быть перемещена или удалена.
             this.driverTableAdapter.Fill(this.rTADataSet.Driver);
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            //dataGridView1.DataSource = Sbind;
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                Sbind.Filter = "";
+            }
+            else
+            {
+                string pole = "";
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0: pole = "Surname"; break;
+                    case 1: pole = "Name"; break;
+                    case 2: pole = "Patronymic"; break;
+                    case 3: pole = "Date_of_birth"; break;
+                    case 4: pole = "Address"; break;
+                    case 5: pole = "Phone_number"; break;
+                    case 6: pole = "Driver_licence"; break;
+                    case 7: pole = "Category"; break;
+                    case 8: pole = "Date_of_issue"; break;
+                    default: pole = "Surname"; break;
+                }
+
+                Sbind.Filter = pole + " like '" + textBox1.Text.ToString() + "%'";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                for (int i = 0; i < dataGridView1.ColumnCount; ++i)
+                {
+                    for (int j = 0; j < dataGridView1.RowCount; ++j)
+                    {
+                        dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.White;
+                        dataGridView1.Rows[j].Cells[i].Style.ForeColor = Color.Black;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < dataGridView1.ColumnCount; ++i)
+                {
+                    for (int j = 0; j < dataGridView1.RowCount; ++j)
+                    {
+                        dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.White;
+                        dataGridView1.Rows[j].Cells[i].Style.ForeColor = Color.Black;
+                    }
+                }
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    for (int i = 0; i < dataGridView1.ColumnCount; ++i)
+                    {
+                        for (int j = 0; j < dataGridView1.RowCount; ++j)
+                        {
+                            var value = dataGridView1.Rows[j].Cells[i].Value;
+                            if (value != null)
+                            {
+                                String baseStr = value.ToString();
+                                if (baseStr.IndexOf(textBox2.Text) > -1)
+                                {
+                                    dataGridView1.Rows[j].Cells[i].Style.BackColor = Color.Yellow;
+                                    dataGridView1.Rows[j].Cells[i].Style.ForeColor = Color.Black;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    int columnID = 0;
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 1: columnID = 0; break;
+                        case 2: columnID = 1; break;
+                        case 3: columnID = 2; break;
+                        case 4: columnID = 3; break;
+                        case 5: columnID = 4; break;
+                        case 6: columnID = 5; break;
+                        case 7: columnID = 6; break;
+                        case 8: columnID = 7; break;
+                        case 9: columnID = 8; break;
+                        default: columnID = 0; break;
+                    }
+
+                    for (int j = 0; j < dataGridView1.RowCount; ++j)
+                    {
+                        var value = dataGridView1.Rows[j].Cells[columnID].Value;
+                        if (value != null)
+                        {
+                            String baseStr = value.ToString();
+                            if (baseStr.IndexOf(textBox2.Text) > -1)
+                            {
+                                dataGridView1.Rows[j].Cells[columnID].Style.BackColor = Color.Yellow;
+                                dataGridView1.Rows[j].Cells[columnID].Style.ForeColor = Color.Black;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
